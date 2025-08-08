39bd5f39d44224fe8c074c01bc09bd97c32b2578
g1tpk47dj <tiktokgokhusus@gmail.com>
2025-08-08 18:01:45 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'bakery-cache-v1';
+const urlsToCache = [
+  'https://www.bayarqris.my.id/p/anita-family-bakery-toko-roti-terbaik.html',
+  'https://pwa-files.vercel.app/icons/icon-192.png',
+  'https://pwa-files.vercel.app/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
